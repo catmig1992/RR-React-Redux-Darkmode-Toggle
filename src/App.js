3e265f10cc5b8526b21ae00610d916f2403f3c7c
Catherine Miguel <catmig1992@gmail.com>
2023-01-09 22:30:09 -0800

    added toggle functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import './App.css';
-import Nav from './components/Nav'
-import ContentWrapper from './components/ContentWrapper'
-import Footer from './components/Footer'
+import "./App.css";
+import Nav from "./components/Nav";
+import ContentWrapper from "./components/ContentWrapper";
+import Footer from "./components/Footer";
+import { useSelector, useDispatch } from "react-redux";
+import { setDarkMode, setLightMode } from "./features/modeSlice";
 
 function App() {
-  
+  const dispatch = useDispatch();
+  const mode = useSelector((state) => state.mode);
+
+  const toggleMode = () => {
+    mode.darkMode ? dispatch(setLightMode()) : dispatch(setDarkMode());
+  };
+
   return (
-    <div style={{ backgroundColor: 'white', color: 'black' }} className="App">
+    <div
+      style={{ backgroundColor: mode.color1, color: "black" }}
+      className="App"
+    >
       <Nav />
+      <button onClick={toggleMode}>
+        {mode.darkMode ? "Light Mode" : "Dark Mode"}
+      </button>
       <ContentWrapper />
       <Footer />
     </div>
